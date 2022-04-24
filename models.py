bc4a1eedb122de4332d6159f1fc031a047d52e52
Brandon <b1ho@ucsd.edu>
2022-04-24 12:05:37 -0700

    models init

---
+++ models.py
@@ -0,0 +1,10 @@
+from torch import *
+
+
+class BiDeepSeq2Seq(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(BiDeepSeq2Seq, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.encoder = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim)
+        self.decoder = nn.LSTM(input_size=output_dim, hidden_size=hidden_dim)
+        self.h2pt = nn.Linear(in_features=hidden_dim, out_features=output_dim)
