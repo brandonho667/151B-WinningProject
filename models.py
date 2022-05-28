3668524da27fb4617d4a0d7c39ae3b33db03ae7b
neelay-j <njogleka@ucsd.edu>
2022-05-27 20:24:57 -0700

    Added LSTMAttention2, didnt work

--- models.py
+++ models.py
@@ -105,6 +105,7 @@ class Conv2Seq(torch.nn.Module):
         self.attn1 = torch.nn.Linear(channel_sizes[-1], hidden_dim)
         self.attn2 = torch.nn.Linear(channel_sizes[-1], hidden_dim)
         self.attn3 = torch.nn.Linear(channel_sizes[-1], hidden_dim)
+        self.attn_drp = torch.nn.Dropout(p=dropout)
         self.softmax = torch.nn.Softmax(dim=1)
             
         self.lstm_dec1 = torch.nn.LSTM(
@@ -144,10 +145,18 @@ class Conv2Seq(torch.nn.Module):
         y = torch.zeros((x.size(0), 60, x.size(2)), device=self.device)
         
         for i in range(60):
+            if self.training:
+                keys1_d = self.attn_drp(keys1)
+                keys2_d = self.attn_drp(keys2)
+                keys3_d = self.attn_drp(keys3)
+            else:
+                keys1_d = keys1
+                keys2_d = keys2
+                keys3_d = keys3
             # perform attention for each LSTM layer
-            align1 = torch.bmm(h1.transpose(0, 1).contiguous(), keys1).transpose(1, 2).contiguous()
-            align2 = torch.bmm(h2.transpose(0, 1).contiguous(), keys2).transpose(1, 2).contiguous()
-            align3 = torch.bmm(h3.transpose(0, 1).contiguous(), keys3).transpose(1, 2).contiguous()
+            align1 = torch.bmm(h1.transpose(0, 1).contiguous(), keys1_d).transpose(1, 2).contiguous()
+            align2 = torch.bmm(h2.transpose(0, 1).contiguous(), keys2_d).transpose(1, 2).contiguous()
+            align3 = torch.bmm(h3.transpose(0, 1).contiguous(), keys3_d).transpose(1, 2).contiguous()
             
             attn1 = self.softmax(align1)
             attn2 = self.softmax(align2)
@@ -176,9 +185,94 @@ class Conv2Seq(torch.nn.Module):
             y[:, i:i+1, :] = self.linear(lstm_dec3_out)
         
         return y
-    
+
+class LSTMAttention2(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, dropout, device):
+        super(LSTMAttention2, self).__init__()
+        
+        self.enc = torch.nn.LSTM(
+            input_size=input_dim, hidden_size=hidden_dim, num_layers=2, dropout=dropout, batch_first=True, bidirectional=True)
+        self.enc_dropout = torch.nn.Dropout(p=dropout)
+        
+        self.hidden_dim = hidden_dim
+        self.attn1 = torch.nn.Linear(hidden_dim*2, hidden_dim*2)
+        self.attn2 = torch.nn.Linear(hidden_dim*2, hidden_dim*2)
+        self.attn3 = torch.nn.Linear(hidden_dim*2, hidden_dim*2)
+        self.attn_drp = torch.nn.Dropout(p=dropout)
+        self.softmax = torch.nn.Softmax(dim=1)
+            
+        self.dec1 = torch.nn.LSTM(
+            input_size=hidden_dim*2+input_dim, hidden_size=hidden_dim*2, batch_first=True)
+        self.drp1 = torch.nn.Dropout(p=dropout)
+        self.dec2 = torch.nn.LSTM(
+            input_size=hidden_dim*4, hidden_size=hidden_dim*2, batch_first=True)
+        self.drp2 = torch.nn.Dropout(p=dropout)
+        self.dec3 = torch.nn.LSTM(
+            input_size=hidden_dim*4, hidden_size=hidden_dim*2, batch_first=True)
+        
+        self.linear = torch.nn.Linear(in_features=hidden_dim*2, out_features=output_dim)
+        self.device = device
+       
+    def forward(self, x, label=None):
+        enc_out, _ = self.enc(x)
+        if self.training:
+            enc_out = self.enc_dropout(enc_out)
+        
+        # cache some values to use throughout loop
+        keys1 = self.attn1(enc_out).transpose(1, 2).contiguous()
+        keys2 = self.attn2(enc_out).transpose(1, 2).contiguous()
+        keys3 = self.attn3(enc_out).transpose(1, 2).contiguous()
+        h1 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        h2 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        h3 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        c1 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        c2 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        c3 = torch.zeros((1, x.size(0), self.hidden_dim*2), device=self.device)
+        y = torch.zeros((x.size(0), 60, x.size(2)), device=self.device)
+        
+        for i in range(60):
+            if self.training:
+                keys1_d = self.attn_drp(keys1)
+                keys2_d = self.attn_drp(keys2)
+                keys3_d = self.attn_drp(keys3)
+            else:
+                keys1_d = keys1
+                keys2_d = keys2
+                keys3_d = keys3
+            # perform attention for each LSTM layer
+            align1 = torch.bmm(h1.transpose(0, 1).contiguous(), keys1_d).transpose(1, 2).contiguous()
+            align2 = torch.bmm(h2.transpose(0, 1).contiguous(), keys2_d).transpose(1, 2).contiguous()
+            align3 = torch.bmm(h3.transpose(0, 1).contiguous(), keys3_d).transpose(1, 2).contiguous()
+            
+            attn1 = self.softmax(align1)
+            attn2 = self.softmax(align2)
+            attn3 = self.softmax(align3)
+            
+            context1 = torch.sum(attn1 * enc_out, dim=1, keepdim=True)
+            context2 = torch.sum(attn2 * enc_out, dim=1, keepdim=True)
+            context3 = torch.sum(attn3 * enc_out, dim=1, keepdim=True)
+            
+            # evaluate cells for each layer once
+            if i == 0:
+                inp1 = x[:, -1, :].unsqueeze(1)
+            elif self.training:
+                inp1 = label[:, i-1, :].unsqueeze(1) #teacher forcing
+            else:
+                inp1 = y[:, i-1, :].unsqueeze(1) #auto regression
+            dec1_out, (h1, c1) = self.dec1(torch.cat((context1, inp1), dim=2), (h1, c1))
+            if self.training:
+                dec1_out = self.drp1(dec1_out)
+            dec2_out, (h2, c2) = self.dec2(torch.cat((context2, dec1_out), dim=2), (h2, c2))
+            if self.training:
+                dec2_out = self.drp2(dec2_out)
+            dec3_out, (h3, c3) = self.dec3(torch.cat((context3, dec2_out), dim=2), (h3, c3))
+            
+            # store output in respective location
+            y[:, i:i+1, :] = self.linear(dec3_out)
+        
+        return y
             
 if __name__ == "__main__":
-    model = Conv2Seq(6, 128, 6, 0, torch.device("cpu"))
+    model = LSTMAttention2(6, 128, 6, 0, torch.device("cpu"))
     model.eval()
-    model(torch.rand((6, 50, 6)))
+    print(model(torch.rand((6, 50, 6)))[:, -5:, :])
